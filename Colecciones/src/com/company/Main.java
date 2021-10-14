bd3287272bf20fa40f9e34e02753b5574c22a3ec
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-14 12:27:06 -0500

    Comenza de uso de colecciones

--- Colecciones/src/com/company/Main.java
+++ Colecciones/src/com/company/Main.java
@@ -1,21 +1,33 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String universidades [];
-        universidades = new String[5];
-        universidades[0]= "ITVH";
-        universidades[1]= "ULA";
-        universidades[2]= "UAM";
-        universidades[3]= "UADM";
-        universidades[4]= "UNAM";
-//        universidades[5]= "IPN";
-//        universidades[6]= "TESCO";
-//        universidades[7]= "UnADM";
-        for (String universidad : universidades){
-            System.out.println(universidad);
-        }
+//        String universidades [];
+//        universidades = new String[5];
+//        universidades[0]= "ITVH";
+//        universidades[1]= "ULA";
+//        universidades[2]= "UAM";
+//        universidades[3]= "UADM";
+//        universidades[4]= "UNAM";
+////        universidades[5]= "IPN";
+////        universidades[6]= "TESCO";
+////        universidades[7]= "UnADM";
+//        for (String universidad : universidades){
+//            System.out.println(universidad);
+//        }
+
+        /*
+        Uso de colecciones
+         */
+        Set<String> frutas = new HashSet<>();
+        //Caracteristicas principales de set
+        /*
+          *No puede contener elementos duplicados
+         */
     }
 }
