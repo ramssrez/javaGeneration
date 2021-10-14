cf0111e707d4cf11645d1ff75e4e9f1670316523
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-14 12:04:43 -0500

    Creación del proyecto de arreglos

--- Refractorizacion/src/com/company/Main.java
+++ Refractorizacion/src/com/company/Main.java
@@ -10,36 +10,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         todosMetodos();
-        /*
-                Scanner console = new Scanner( System.in);
-        System.out.print("Enter addition number 1  ");
-        int num1 = console.nextInt();
-        System.out.print("Enter addition number 2  ");
-        int num2 = console.nextInt();
-        int addition = num1 + num2;
-        System.out.println( num1+" + "+num2+" = "+ addition);
-
-        System.out.print("Enter subtraction number 1  ");
-        num1 = console.nextInt();
-        System.out.print("Enter subtraction number 2  ");
-        num2 = console.nextInt();
-        int subtraction = num1 - num2;
-        System.out.println( num1+" - "+num2+" = "+ subtraction);
-
-        System.out.print("Enter multiplication number 1  ");
-        num1 = console.nextInt();
-        System.out.print("Enter multiplication number 2  ");
-        num2 = console.nextInt();
-        int multiplication = num1 * num2;
-        System.out.println( num1+" * "+num2+" = "+ multiplication);
-
-        System.out.print("Enter division number 1  ");
-        num1 = console.nextInt();
-        System.out.print("Enter division number 2  ");
-        num2 = console.nextInt();
-        int division = num1 / num2;
-        System.out.println( num1+" / "+num2+" = "+ division);
-         */
     }
     public static void todosMetodos(){
         ingresoValores();
