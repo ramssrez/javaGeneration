d0e83726a1c3fd57a7e3ade36baa28ea7060a743
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-14 12:25:13 -0500

    Uso de arreglos para strings

--- Colecciones/src/com/company/Main.java
+++ Colecciones/src/com/company/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String universidades [];
+        universidades = new String[5];
+        universidades[0]= "ITVH";
+        universidades[1]= "ULA";
+        universidades[2]= "UAM";
+        universidades[3]= "UADM";
+        universidades[4]= "UNAM";
+//        universidades[5]= "IPN";
+//        universidades[6]= "TESCO";
+//        universidades[7]= "UnADM";
+        for (String universidad : universidades){
+            System.out.println(universidad);
+        }
     }
 }
