c6a53f452a6d1b8d11546714be6f2ca456707aaa
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-14 13:05:55 -0500

    Vista de la velocidad de los diferentes opciones de la estructura de datos

--- Colecciones/src/com/company/Main.java
+++ Colecciones/src/com/company/Main.java
@@ -82,5 +82,32 @@ public class Main {
         for (String fruta : frutasTres){
             System.out.println(fruta);
         }
+
+        System.out.println("--------------------------------");
+        System.out.println("Raúl Ramírez Pérez");
+
+        final Set hashSet = new HashSet(1_000_000);
+        final Long startHashSetTime = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            hashSet.add(i);
+        }
+        final Long endHashSetTime = System.currentTimeMillis();
+        System.out.println("Time spent by HashSet: " + (endHashSetTime - startHashSetTime));
+
+        final Set treeSet = new TreeSet();
+        final Long startTreeSetTime = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            treeSet.add(i);
+        }
+        final Long endTreeSetTime = System.currentTimeMillis();
+        System.out.println("Time spent by TreeSet: " + (endTreeSetTime - startTreeSetTime));
+
+        final Set linkedHashSet = new LinkedHashSet(1_000_000);
+        final Long startLinkedHashSetTime = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            linkedHashSet.add(i);
+        }
+        final Long endLinkedHashSetTime = System.currentTimeMillis();
+        System.out.println("Time spent by LinkedHashSet: " + (endLinkedHashSetTime - startLinkedHashSetTime));
     }
 }
