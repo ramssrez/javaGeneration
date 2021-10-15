f0fb522ec09e96b989c64063dd3b733d82d67422
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-15 10:26:54 -0500

    Creación de la clase perro

---
+++ POOJava/src/com/company/Perro.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Perro {
+    /*private*/ String name;
+    /*private*/ String raza;
+    /*private*/ Integer edad;
+    /*private*/ String size;
+
+    public void caracteristicas(){
+        System.out.println("Hola mi nombre es: " + this.name + ", soy de raza " + this.raza +
+                " tengo " + this.edad + " soy de tamaño " + this.size);
+    }
+    public void comer(){
+        System.out.println("Estoy comiendo");
+    }
+    public void dormir(){
+        System.out.println("zzzzzzzzzzz");
+    }
+    public void jugar(){
+        System.out.println("Estoyj ugando");
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" +
+                "name='" + name + '\'' +
+                ", raza='" + raza + '\'' +
+                ", edad=" + edad +
+                ", size='" + size + '\'' +
+                '}';
+    }
+}
