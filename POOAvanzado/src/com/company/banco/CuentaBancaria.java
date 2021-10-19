81e96d0423c82ffe25b50eae84c9afe89e6b1ae6
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-19 15:58:47 -0500

    Creación de los metodos set

--- POOAvanzado/src/com/company/banco/CuentaBancaria.java
+++ POOAvanzado/src/com/company/banco/CuentaBancaria.java
@@ -14,6 +14,19 @@ public class CuentaBancaria {
     public float getSaldo(){
         return this.saldo;
     }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setClave(String clave){
+        this.clave = clave;
+    }
+    public void setSaldo(float saldo){
+        if (saldo<0){
+            this.saldo = 0;
+        }else{
+            this.saldo = saldo;
+        }
+    }
     public void  mostrarSaldo(){
         System.out.println(this.saldo);
     }
