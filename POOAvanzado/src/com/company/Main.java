d533282e3c219b12ea09a25e707bca96d45a1481
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-19 15:49:54 -0500

    Integración de los metodos getters

--- POOAvanzado/src/com/company/Main.java
+++ POOAvanzado/src/com/company/Main.java
@@ -1,18 +1,25 @@
 package com.company;
 
+import com.company.banco.CuentaBancaria;
 import com.company.paquete2.Animal;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Persona persona = new Persona();
-        //Persona persona = new Persona();
-//        persona.nombre = "Raúl";
-//        persona.edad = 29;
-        Animal animal = new Animal();
-        //No se puede ingresar desde un paquete poder el tipo default a menos que se le cambie el modificaor
-        animal.nombre = "Iguana";
+        CuentaBancaria cuenta = new CuentaBancaria();
+        cuenta.nombre = "Raúl Rampírez";
+
+
+
+
+//        Persona persona = new Persona();
+//        //Persona persona = new Persona();
+////        persona.nombre = "Raúl";
+////        persona.edad = 29;
+//        Animal animal = new Animal();
+//        //No se puede ingresar desde un paquete poder el tipo default a menos que se le cambie el modificaor
+//        animal.nombre = "Iguana";
 
     }
 }
--- POOAvanzado/src/com/company/banco/CuentaBancaria.java
+++ POOAvanzado/src/com/company/banco/CuentaBancaria.java
@@ -1,4 +1,20 @@
 package com.company.banco;
 
 public class CuentaBancaria {
+    private String nombre;
+    private String clave;
+    private float saldo;
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getClave(){
+        return this.clave;
+    }
+    public float getSaldo(){
+        return this.saldo;
+    }
+    public void  mostrarSaldo(){
+        System.out.println(this.saldo);
+    }
 }
