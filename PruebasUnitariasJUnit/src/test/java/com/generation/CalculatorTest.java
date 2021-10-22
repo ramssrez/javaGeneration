300866b037c9fa63518d26766f2a9c57eff58f8c
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-22 15:31:23 -0500

    Metodos para la generación de test

--- PruebasUnitariasJUnit/src/test/java/com/generation/CalculatorTest.java
+++ PruebasUnitariasJUnit/src/test/java/com/generation/CalculatorTest.java
@@ -1,5 +1,37 @@
 package com.generation;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
-    
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void addTest()
+    {
+        assertEquals( 8, calculator.addNumbers( 5, 3 ) );
+    }
+
+
+    @Test
+    public void subtractTest()
+    {
+        assertEquals( 5, calculator.subtractNumbers( 10, 5 ) );
+    }
+
+
+    @Test
+    public void multiplyTest()
+    {
+        assertEquals( 15, calculator.multiplyNumbers( 5, 3 ) );
+    }
+
+
+    @Test
+    public void divideTest()
+    {
+        assertEquals( 5.0, calculator.divideNumbers( 5, 1 ) );
+    }
+
 }
