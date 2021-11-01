522d24d7d4c491356a05f4d478b040a911193b96
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-01 16:02:23 -0600

    Implementación para retornar una lista de objetos y el total

--- UserServiceApplicaiton/src/main/java/org/generation/MyFirstWebApp/controller/UserController.java
+++ UserServiceApplicaiton/src/main/java/org/generation/MyFirstWebApp/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserController
 {
@@ -15,6 +17,9 @@ public class UserController
     {
         this.userService = userService;
         userService.add( new User( 1, "Santiago", "Carrillo" ) );
+        userService.add( new User( 2, "Juan", "Sanchez" ) );
+        userService.add( new User( 3, "Pedro", "Ortiz" ) );
+
     }
 
     @GetMapping( "/user" )
@@ -23,4 +28,13 @@ public class UserController
         return userService.getUser( 0 );
     }
 
+    @GetMapping("/users/all")
+    public int totalUsers() {
+        return userService.size();
+    }
+
+    @GetMapping("/users/list")
+    public List<User> totalUsersList() {
+        return userService.getAll();
+    }
 }
