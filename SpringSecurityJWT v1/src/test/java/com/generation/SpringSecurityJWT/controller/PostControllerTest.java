78a503f5714b5404894178f9edda177be1aa67ff
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-10 12:05:14 -0600

    Intehgración del test de postController

--- SpringSecurityJWT v1/src/test/java/com/generation/SpringSecurityJWT/controller/PostControllerTest.java
+++ SpringSecurityJWT v1/src/test/java/com/generation/SpringSecurityJWT/controller/PostControllerTest.java
@@ -3,11 +3,45 @@ package com.generation.SpringSecurityJWT.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class PostControllerTest {
     @Test
     @DisplayName("Probando mockito")
+    void testMockito(){
+        //mock creation
+        List mockedList = mock(List.class);
 
+        //using mock object
+        mockedList.add("one");
+        mockedList.clear();
+
+        //verification
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
+
+    }
+
+    @Test
+    @DisplayName("Probando mockito v2")
+    void testMockito2(){
+
+        // Crea un objeto simulado, el parámetro puede ser una clase o una interfaz
+        List<String> list = mock(List.class);
+
+        // Establecer el valor de retorno esperado del método
+        when(list.get(0)).thenReturn("helloworld");
+
+        String result = list.get(0);
+
+        // Verifica la llamada al método (si se llama a get (0))
+        verify(list).get(0);
+
+        // prueba junit
+        assertEquals("helloworld", result);
+    }
 
 }
