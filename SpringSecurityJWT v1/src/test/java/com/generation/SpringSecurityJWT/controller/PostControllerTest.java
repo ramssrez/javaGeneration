c1aa51007d1979eeb03676a5b39fd542effa92c1
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-10 11:42:43 -0600

    Creación de la clase para la prueba unitaria

---
+++ SpringSecurityJWT v1/src/test/java/com/generation/SpringSecurityJWT/controller/PostControllerTest.java
@@ -0,0 +1,13 @@
+package com.generation.SpringSecurityJWT.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+    @Test
+    @DisplayName("Probando mockito")
+
+
+}
